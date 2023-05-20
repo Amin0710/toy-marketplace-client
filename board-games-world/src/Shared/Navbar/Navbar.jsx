d698b33e2e2e4c917fea6d8c4ac6806d26df5d9c
Nurul Amin <nurul.amin.naim@gmail.com>
2023-05-21 06:21:15 +1000

    Semi done more to do

---
+++ board-games-world/src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/boredom-busters-board-games-logo.png";
+
+const Navbar = () => {
+	return (
+		<div className="navbar bg-base-100">
+			<div className="flex-1">
+				<Link to="/" className="btn btn-ghost normal-case text-xl">
+					<img
+						src={logo}
+						height="40px"
+						width="40px"
+						alt=""
+						className="rounded"
+					/>
+					Boredom Busters Board Games
+				</Link>
+			</div>
+			<div className="flex-none">
+				<ul className="menu menu-horizontal px-1">
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/">All Toys</Link>
+					</li>
+					<li>
+						<Link to="/">Blogs</Link>
+					</li>
+				</ul>
+				<div className="dropdown dropdown-end">
+					<label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+						<div className="w-10 rounded-full">
+							<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+						</div>
+					</label>
+					<ul
+						tabIndex={0}
+						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+						<li>
+							<Link to="/">My Toys</Link>
+						</li>
+						<li>
+							<Link to="/">Add A Toy</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
