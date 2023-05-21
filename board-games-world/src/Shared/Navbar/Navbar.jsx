4761e22f8088f06e2da7c8e004ad04392730a6a0
Nurul Amin <nurul.amin.naim@gmail.com>
2023-05-21 19:26:03 +1000

    nav bar done

--- board-games-world/src/Shared/Navbar/Navbar.jsx
+++ board-games-world/src/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/boredom-busters-board-games-logo.png";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Navbar = () => {
+	const { user, loading, logOut } = useContext(AuthContext);
+
+	const handleMouseOver = () => {
+		const userName = document.getElementById("userName");
+		userName.classList.remove("invisible");
+	};
+
+	const handleMouseOut = () => {
+		const userName = document.getElementById("userName");
+		userName.classList.add("invisible");
+	};
+
 	return (
 		<div className="navbar bg-base-100">
 			<div className="flex-1">
@@ -28,28 +42,52 @@ const Navbar = () => {
 						<Link to="/">Blogs</Link>
 					</li>
 				</ul>
-				<ul className="menu menu-horizontal px-1">
-					<li>
-						<Link to="/login">Login</Link>
-					</li>
-				</ul>
-				<div className="dropdown dropdown-end">
-					<label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-						<div className="w-10 rounded-full">
-							<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-						</div>
-					</label>
-					<ul
-						tabIndex={0}
-						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-						<li>
-							<Link to="/">My Toys</Link>
-						</li>
-						<li>
-							<Link to="/">Add A Toy</Link>
-						</li>
-					</ul>
-				</div>
+				{loading ? (
+					<div className="flex justify-center">
+						<button
+							className="bg-warning text-white px-4 py-2 rounded-md flex items-center"
+							disabled>
+							<span className="animate-spin mr-2 h-4 w-4 border-t-2 border-b-2 border-white rounded-full"></span>
+							Loading...
+						</button>
+					</div>
+				) : (
+					!!user || (
+						<ul className="menu menu-horizontal px-1">
+							<li>
+								<Link to="/login">Login</Link>
+							</li>
+						</ul>
+					)
+				)}
+				{!!user && (
+					<div className="dropdown dropdown-end">
+						<label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+							<div
+								className="w-10 rounded-full"
+								onMouseOver={handleMouseOver}
+								onMouseOut={handleMouseOut}>
+								<img src={user?.photoURL} />
+							</div>
+							<p className="mt-5 invisible" id="userName">
+								{user?.displayName}
+							</p>
+						</label>
+						<ul
+							tabIndex={0}
+							className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+							<li>
+								<Link to="/">My Toys</Link>
+							</li>
+							<li>
+								<Link to="/">Add A Toy</Link>
+							</li>
+							<li>
+								<button onClick={logOut}>Log Out</button>
+							</li>
+						</ul>
+					</div>
+				)}
 			</div>
 		</div>
 	);
