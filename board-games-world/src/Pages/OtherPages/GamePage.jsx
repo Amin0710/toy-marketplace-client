151e554c7d0fb24e361353e4dc7ac91371b7d004
Nurul Amin <nurul.amin.naim@gmail.com>
2023-05-24 13:39:45 +1000

    game page done

--- board-games-world/src/Pages/OtherPages/GamePage.jsx
+++ board-games-world/src/Pages/OtherPages/GamePage.jsx
@@ -1,5 +1,46 @@
+import { useLoaderData } from "react-router-dom";
+
 const GamePage = () => {
-	return <div></div>;
+	const game = useLoaderData();
+
+	return (
+		<div className="flex items-center justify-center mb-8">
+			<div className=" container hero min-h-fit bg-gray-200 rounded">
+				<div className="hero-content flex-col lg:flex-row-reverse">
+					<img
+						src={game.picture_url}
+						className="max-w-sm rounded-lg shadow-2xl"
+					/>
+					<div>
+						<h1 className="text-5xl font-bold">{game.game_name}</h1>
+						<p className="py-2 text-purple-700">
+							Seller Name: {game.seller_name}
+						</p>
+						<p className="py-2 text-purple-700">
+							Seller Email: {game.seller_email}
+						</p>
+						<p className="py-2 text-purple-700">Price: ${game.price}</p>
+						<p className="py-2 text-purple-700">Rating: {game.rating}</p>
+						<p className="py-2 text-purple-700">
+							Available Quantity: {game.available_quantity}
+						</p>
+						<button className="btn btn-primary">
+							<label htmlFor="my-modal-4">Detail description</label>
+						</button>
+
+						{/* Modal */}
+						<input type="checkbox" id="my-modal-4" className="modal-toggle" />
+						<label htmlFor="my-modal-4" className="modal cursor-pointer">
+							<label className="modal-box relative" htmlFor="">
+								<h3 className="text-lg font-bold">{game.game_name}</h3>
+								<p className="py-4">{game.detail_description}</p>
+							</label>
+						</label>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default GamePage;
